ae6ed64c15dec3d903bcbbc2132179e416ea65d9
eklownr <eklownr@gmail.com>
2026-03-02 13:36:27 +0100

    add server.ts and ts-node to package.json

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,24 @@
-import {exptess} from 'express';
+import express from 'express';
 
+const app = express();
+app.use(express.json());
+const PORT: number = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running: ${PORT}`)
+})
+
+let recipes = [
+    {id: 1, name: "American Pancakes", cuisine: "USA", prepTime: "15 min" },
+    {id: 2, name: "Fattiga riddare", cuisine: "Sweden", prepTime: "20 min" },
+    {id: 3, name: "Flygande Jackob", cuisine: "Sweden", prepTime: "35 min" },
+    {id: 4, name: "Tunnbröd bröd", cuisine: "Sweden", prepTime: "90 min" },
+];
+
+app.get("/recipes", (req, res) =>{
+    res.json(recipes)
+});
+
+app.get("/recipes:id", (req, rep) =>{
+
+});
